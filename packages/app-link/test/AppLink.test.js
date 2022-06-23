1e0b0215622dc76d0a38bf917ae04fc3a01eb7ab
Ico Lizhen <icolizhen@gmail.com>
2022-06-23 15:34:31 +0200

    feat: add test to check if there is a slot inside link

--- packages/app-link/test/AppLink.test.js
+++ packages/app-link/test/AppLink.test.js
@@ -28,4 +28,16 @@ describe('AppLink', () => {
 
     expect(navigatorSpy.calledWith('/my-villagers')).to.be.true;
   });
+
+  it('should render a slot inside the link', async () => {
+    const element = await scopedFixture(
+      html`<app-link href="/my-villagers"></app-link>`
+    );
+
+    const link = element.shadowRoot.querySelector('a');
+
+    const slotElement = link.querySelector('slot');
+
+    expect(slotElement).to.exist;
+  });
 });
