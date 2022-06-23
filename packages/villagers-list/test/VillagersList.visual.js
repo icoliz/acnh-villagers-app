5016d373c42bb55d7956e0638eec9fc656c14f1c
Ico Lizhen <icolizhen@gmail.com>
2022-06-23 14:41:19 +0200

    fix: use single quotes

--- packages/villagers-list/test/VillagersList.visual.js
+++ packages/villagers-list/test/VillagersList.visual.js
@@ -1,41 +1,41 @@
-import { visualDiff } from "@web/test-runner-visual-regression";
-import { html, aTimeout, fixture, waitUntil } from "@open-wc/testing";
+import { visualDiff } from '@web/test-runner-visual-regression';
+import { html, aTimeout, fixture, waitUntil } from '@open-wc/testing';
 
-import { VillagersList } from "../index.js";
+import { VillagersList } from '../index.js';
 
 const villagers = [
   {
-    birthday: "11/4",
-    birthday_string: "April 11th",
-    gender: "Male",
-    icon: "/.web-test-runner/assets/img/punchy.png",
+    birthday: '11/4',
+    birthday_string: 'April 11th',
+    gender: 'Male',
+    icon: '/.web-test-runner/assets/img/punchy.png',
     id: 48,
-    image: "/.web-test-runner/assets/img/punchy.png",
-    nameCN: "尔光",
-    nameDE: "Julian",
-    nameEN: "Punchy",
-    nameES: "Félix",
-    personality: "Lazy",
-    species: "Cat",
+    image: '/.web-test-runner/assets/img/punchy.png',
+    nameCN: '尔光',
+    nameDE: 'Julian',
+    nameEN: 'Punchy',
+    nameES: 'Félix',
+    personality: 'Lazy',
+    species: 'Cat',
   },
   {
-    birthday: "17/3",
-    birthday_string: "March 17th",
-    gender: "Female",
-    icon: "/.web-test-runner/assets/img/cheri.png",
+    birthday: '17/3',
+    birthday_string: 'March 17th',
+    gender: 'Female',
+    icon: '/.web-test-runner/assets/img/cheri.png',
     id: 74,
-    image: "/.web-test-runner/assets/img/cheri.png",
-    nameCN: "樱桃",
-    nameDE: "Claudia",
-    nameEN: "Cheri",
-    nameES: "Cerecita",
-    personality: "Peppy",
-    species: "Cub",
+    image: '/.web-test-runner/assets/img/cheri.png',
+    nameCN: '樱桃',
+    nameDE: 'Claudia',
+    nameEN: 'Cheri',
+    nameES: 'Cerecita',
+    personality: 'Peppy',
+    species: 'Cub',
   },
 ];
 
-describe("VillagersList", () => {
-  const scopedElements = { "villagers-list": VillagersList };
+describe('VillagersList', () => {
+  const scopedElements = { 'villagers-list': VillagersList };
   const scopedFixture = (template) =>
     fixture(
       html`<div style="background-color: #fff; padding: 16px">
@@ -44,12 +44,12 @@ describe("VillagersList", () => {
       { scopedElements }
     );
 
-  it("should render villagers list with villagers", async () => {
+  it('should render villagers list with villagers', async () => {
     const element = await scopedFixture(
       html`<villagers-list .villagers=${villagers}></villagers-list>`
     );
 
-    const villagersListEl = element.querySelector("villagers-list");
+    const villagersListEl = element.querySelector('villagers-list');
     await aTimeout(200);
 
     const punchyInfo = villagersListEl.shadowRoot.querySelector(
@@ -69,14 +69,14 @@ describe("VillagersList", () => {
     await waitUntil(() => punchyImg);
     await waitUntil(() => cheriImg);
 
-    await visualDiff(element, "villagers-list/with-villagers");
+    await visualDiff(element, 'villagers-list/with-villagers');
   });
 
-  it("should render villagers list without villagers", async () => {
+  it('should render villagers list without villagers', async () => {
     const element = await scopedFixture(
       html`<villagers-list></villagers-list>`
     );
 
-    await visualDiff(element, "villagers-list/without-villagers");
+    await visualDiff(element, 'villagers-list/without-villagers');
   });
 });
