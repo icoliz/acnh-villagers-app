c41c467e57a9308ab0bc504c2473b9424cc2da66
Ico Lizhen <icolizhen@gmail.com>
2022-06-23 09:29:15 +0200

    fix: delete storybook villager

--- packages/villager-info/stories/VillagerInfo.stories.js
+++ packages/villager-info/stories/VillagerInfo.stories.js
@@ -4,21 +4,6 @@ import { VillagerInfo } from '../index.js';
 
 window.customElements.define('villager-info', VillagerInfo);
 
-const villager = {
-  birthday: '9/3',
-  birthday_string: 'March 9th',
-  gender: 'Male',
-  icon: 'https://acnhapi.com/v1/icons/villagers/1',
-  id: 1,
-  image: 'https://acnhapi.com/v1/images/villagers/1',
-  nameCN: '阳明',
-  nameDE: 'Theo',
-  nameEN: 'Cyrano',
-  nameES: 'Cirano',
-  personality: 'Cranky',
-  species: 'Anteater',
-};
-
 export default {
   title: 'VillagerInfo',
 };
@@ -44,9 +29,9 @@ WithVillager.args = {
     species: 'Anteater',
   },
   showWishlistButton: true,
-  isInWishlist: true,
+  isInWishlist: false,
   showMyVillagersButton: true,
-  isInMyVillagersList: true,
+  isInMyVillagersList: false,
 };
 
 export const WithoutVillager = () => html`<villager-info></villager-info>`;
