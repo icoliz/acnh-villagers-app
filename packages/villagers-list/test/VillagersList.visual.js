caacf5cf1e14259c8533686f28429eda26cdfb26
Ico Lizhen <icolizhen@gmail.com>
2022-06-23 14:07:54 +0200

    fix: use scoped fixture for visual tests

--- packages/villagers-list/test/VillagersList.visual.js
+++ packages/villagers-list/test/VillagersList.visual.js
@@ -1,74 +1,82 @@
-import { visualDiff } from '@web/test-runner-visual-regression';
-import { waitUntil } from '@open-wc/testing';
-import { VillagersList } from '../index.js';
+import { visualDiff } from "@web/test-runner-visual-regression";
+import { html, aTimeout, fixture, waitUntil } from "@open-wc/testing";
 
-if (!customElements.get('villagers-list')) {
-  customElements.define('villagers-list', VillagersList);
-}
+import { VillagersList } from "../index.js";
 
 const villagers = [
   {
-    birthday: '11/4',
-    birthday_string: 'April 11th',
-    gender: 'Male',
-    icon: '/.web-test-runner/assets/img/punchy.png',
+    birthday: "11/4",
+    birthday_string: "April 11th",
+    gender: "Male",
+    icon: "/.web-test-runner/assets/img/punchy.png",
     id: 48,
-    image: '/.web-test-runner/assets/img/punchy.png',
-    nameCN: '尔光',
-    nameDE: 'Julian',
-    nameEN: 'Punchy',
-    nameES: 'Félix',
-    personality: 'Lazy',
-    species: 'Cat',
+    image: "/.web-test-runner/assets/img/punchy.png",
+    nameCN: "尔光",
+    nameDE: "Julian",
+    nameEN: "Punchy",
+    nameES: "Félix",
+    personality: "Lazy",
+    species: "Cat",
   },
   {
-    birthday: '17/3',
-    birthday_string: 'March 17th',
-    gender: 'Female',
-    icon: '/.web-test-runner/assets/img/cheri.png',
+    birthday: "17/3",
+    birthday_string: "March 17th",
+    gender: "Female",
+    icon: "/.web-test-runner/assets/img/cheri.png",
     id: 74,
-    image: '/.web-test-runner/assets/img/cheri.png',
-    nameCN: '樱桃',
-    nameDE: 'Claudia',
-    nameEN: 'Cheri',
-    nameES: 'Cerecita',
-    personality: 'Peppy',
-    species: 'Cub',
+    image: "/.web-test-runner/assets/img/cheri.png",
+    nameCN: "樱桃",
+    nameDE: "Claudia",
+    nameEN: "Cheri",
+    nameES: "Cerecita",
+    personality: "Peppy",
+    species: "Cub",
   },
 ];
 
-describe('VillagersList', () => {
-  it('should render villagers list with villagers', async () => {
-    const element = document.createElement('div');
-    document.body.appendChild(element);
-    element.style = 'background-color: #fff; padding: 16px';
+describe("VillagersList", () => {
+  const scopedElements = { "villagers-list": VillagersList };
+  const scopedFixture = (template) =>
+    fixture(
+      html`<div style="background-color: #fff; padding: 16px">
+        ${template}
+      </div>`,
+      { scopedElements }
+    );
 
-    const villagersListEl = document.createElement('villagers-list');
-    villagersListEl.villagers = villagers;
-    element.appendChild(villagersListEl);
-    await waitUntil(() => villagersListEl.villagers);
+  it("should render villagers list with villagers", async () => {
+    const element = await scopedFixture(
+      html`<villagers-list .villagers=${villagers}></villagers-list>`
+    );
+
+    const villagersListEl = element.querySelector("villagers-list");
+    await aTimeout(200);
 
     const punchyInfo = villagersListEl.shadowRoot.querySelector(
+      '[data-testid="villager-info-48"]'
+    );
+    const cheriInfo = villagersListEl.shadowRoot.querySelector(
       '[data-testid="villager-info-48"]'
     );
 
     const punchyImg = punchyInfo.shadowRoot.querySelector(
       '[data-testid="villager-img"]'
     );
+    const cheriImg = cheriInfo.shadowRoot.querySelector(
+      '[data-testid="villager-img"]'
+    );
 
-    await waitUntil(() => punchyImg.src);
+    await waitUntil(() => punchyImg);
+    await waitUntil(() => cheriImg);
 
-    await visualDiff(element, 'villagers-list/with-villagers');
+    await visualDiff(element, "villagers-list/with-villagers");
   });
 
-  it('should render villagers list without villagers', async () => {
-    const element = document.createElement('div');
-    document.body.appendChild(element);
-    element.style = 'background-color: #fff; padding: 16px';
+  it("should render villagers list without villagers", async () => {
+    const element = await scopedFixture(
+      html`<villagers-list></villagers-list>`
+    );
 
-    const villagersListEl = document.createElement('villagers-list');
-    element.appendChild(villagersListEl);
-
-    await visualDiff(element, 'villagers-list/without-villagers');
+    await visualDiff(element, "villagers-list/without-villagers");
   });
 });
