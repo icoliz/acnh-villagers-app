59bd3d0addb9580be736ae741e9c7fa2c8ce070a
Ico Lizhen <icolizhen@gmail.com>
2022-06-28 11:36:26 +0200

    fix(test): use scoped fixture and update screenshot of footer

--- packages/acnh-footer/test/AcnhFooter.visual.js
+++ packages/acnh-footer/test/AcnhFooter.visual.js
@@ -1,15 +1,20 @@
+import { html, fixture } from '@open-wc/testing';
 import { visualDiff } from '@web/test-runner-visual-regression';
 import { AcnhFooter } from '../index.js';
 
-if (!customElements.get('acnh-footer')) {
-  customElements.define('acnh-footer', AcnhFooter);
-}
-
 describe('AcnhFooter', () => {
-  it('should render header', async () => {
-    const element = document.createElement('acnh-footer');
-    document.body.appendChild(element);
+  const scopedElements = { 'acnh-footer': AcnhFooter };
+  const scopedFixture = (template) =>
+    fixture(
+      html`<div style="background-color: #fff; padding: 16px">
+        ${template}
+      </div>`,
+      { scopedElements }
+    );
 
-    await visualDiff(element, 'acnh-footer');
+  it('should render footer', async () => {
+    const element = await scopedFixture(html`<acnh-footer></acnh-footer>`);
+
+    await visualDiff(element, 'acnh-footer/acnh-footer');
   });
 });
