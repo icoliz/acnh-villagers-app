5b60324224f02766db78adcfaaf7d7b44890156e
Ico Lizhen <icolizhen@gmail.com>
2022-07-05 17:12:33 +0200

    feat: add local storage and button functionality

--- packages/villagers-list/src/VillagersList.js
+++ packages/villagers-list/src/VillagersList.js
@@ -3,8 +3,11 @@ import { ScopedElementsMixin } from '@open-wc/scoped-elements';
 
 import { VillagerInfo } from '../../villager-info/index.js';
 import { styles } from './VillagersList.styles.js';
+import { LocalStorageController } from '../../controller/localStorage.js';
 
 export class VillagersList extends ScopedElementsMixin(LitElement) {
+  controller = new LocalStorageController(this);
+
   static get scopedElements() {
     return {
       'villager-info': VillagerInfo,
@@ -25,6 +28,45 @@ export class VillagersList extends ScopedElementsMixin(LitElement) {
     super();
 
     this.villagers = [];
+    this.wishlist = this.controller.get('wishlist', []);
+  }
+
+  isVillagerInWishlist(selectedVillager) {
+    const villagerInWishlist = this.wishlist.find(
+      (villager) => villager.id === selectedVillager.id
+    );
+
+    if (villagerInWishlist !== undefined) {
+      return true;
+    }
+    return false;
+  }
+
+  addWishlist(clickedVillager) {
+    if (!this.isVillagerInWishlist(clickedVillager)) {
+      this.wishlist = [...this.wishlist, clickedVillager];
+      this.controller.set('wishlist', this.wishlist);
+    }
+
+    this.requestUpdate();
+  }
+
+  removeWishlist(clickedVillager) {
+    console.log(
+      `clicked remove from wishlist, villager id: ${clickedVillager.id}`
+    );
+  }
+
+  addMyVillager(clickedVillager) {
+    console.log(
+      `clicked add to my villagers, villager id: ${clickedVillager.id}`
+    );
+  }
+
+  removeMyVillager(clickedVillager) {
+    console.log(
+      `clicked remove from my villagers, villager id: ${clickedVillager.id}`
+    );
   }
 
   render() {
@@ -40,6 +82,14 @@ export class VillagersList extends ScopedElementsMixin(LitElement) {
               <villager-info
                 .villager=${villager}
                 data-testid="villager-info-${villager.id}"
+                @add-wishlist=${() => this.addWishlist(villager)}
+                @remove-wishlist=${() => this.removeWishlist(villager)}
+                @add-my-villager=${() => this.addMyVillager(villager)}
+                @remove-my-villager=${() => this.removeMyVillager(villager)}
+                .showWishlistButton=${true}
+                .isInWishlist=${this.isVillagerInWishlist(villager)}
+                .showMyVillagersButton=${true}
+                .isInMyVillagersList=${false}
               ></villager-info>
             </li>
           `
